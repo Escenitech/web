544479c29d5ee3c05cf17fbeec38f71957b118ae
David Sánchez González <david@escenit.com>
2022-03-23 10:44:58 +0100

    updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,14 @@ const packageJson = require('../../package.json');
 export const environment = {
   appName: 'Escenitech',
   owner: 'Escenit Media S.L.',
-  envName: 'PROD',
+  envName: 'DEV',
   themeName: 'default-theme',
   production: true,
   test: false,
   i18nPrefix: '',
   versions: {
     app: packageJson.version,
-  }
+  },
+  languages: ['ca', 'es', 'en'],
+  defaultLanguage: 'es'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 const packageJson = require('../../package.json');
 
 export const environment = {
@@ -10,10 +5,12 @@ export const environment = {
   owner: 'Escenit Media S.L.',
   envName: 'DEV',
   themeName: 'default-theme',
-  production: false,
+  production: true,
   test: false,
   i18nPrefix: '',
   versions: {
-    app: packageJson.version
-  }
+    app: packageJson.version,
+  },
+  languages: ['ca', 'es', 'en'],
+  defaultLanguage: 'es'
 };
