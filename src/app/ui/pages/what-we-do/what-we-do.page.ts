605ac69cb53e88f57fe3bf263f4a65dffcd36732
David Sánchez González <david@escenit.com>
2022-03-24 12:45:57 +0100

    reordered and refactored home and what we do pages

--- src/app/ui/pages/what-we-do/what-we-do.page.ts
+++ src/app/ui/pages/what-we-do/what-we-do.page.ts
@@ -7,19 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhatWeDoPage implements OnInit {
 
-  oldProcessesImg: string = 'assets/icon-oldProcesses.png';
-  newProcessesImg: string = 'assets/icon-newProcesses.png';
-  arrowRightImg: string = 'assets/arrow-right.png';
-  checkImg: string = 'assets/check.png';
-
-  advantagesList = [
-    'app.what-we-do.page.point1.advantages',
-    'app.what-we-do.page.point2.advantages',
-    'app.what-we-do.page.point3.advantages',
-    'app.what-we-do.page.point4.advantages',
-    'app.what-we-do.page.point5.advantages'
-  ];
-
   constructor() { }
 
   ngOnInit(): void {
