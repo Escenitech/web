618ab2b99db8ca9f5f52a8ed4a1b2e6d453bd3ce
David Sánchez González <david@escenit.com>
2022-03-25 11:09:02 +0100

    redone what we do page

--- src/app/ui/pages/what-we-do/what-we-do.page.ts
+++ src/app/ui/pages/what-we-do/what-we-do.page.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhatWeDoPage implements OnInit {
 
+  backEndImgs: string[] = [
+    'assets/tech/php.png',
+    'assets/tech/symfony.png'
+  ];
+
+  frontEndImgs: string[] = [
+    'assets/tech/react.png',
+    'assets/tech/Angular.png',
+    'assets/tech/js.png',
+    'assets/tech/html5.png'
+  ];
+
+  mobileImgs: string[] = [
+    'assets/tech/react.png',
+    'assets/tech/ionic.png'
+  ];
+
+  cloudImgs: string[] = [
+    'assets/tech/aws.png',
+    'assets/tech/googleCloud.png',
+    'assets/tech/firestore.png'
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
