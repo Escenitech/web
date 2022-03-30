298749d92eebd7d127d732c5ba4c2eb75a24b335
David Sánchez González <david@escenit.com>
2022-03-30 10:47:10 +0200

    basic web solution component finished

---
+++ src/app/ui/components/web-solution/web-solution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-web-solution',
+  templateUrl: './web-solution.component.html',
+  styleUrls: ['./web-solution.component.scss']
+})
+export class WebSolutionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
