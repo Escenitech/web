c418c2c369113247a7f1638eb9c9c9d5ff0570e2
David Sánchez González <david@escenit.com>
2022-04-25 12:11:40 +0200

    added animation to background image in home, fixed background images resizing in home

--- src/app/shared/animate-on-scroll.ts
+++ src/app/shared/animate-on-scroll.ts
@@ -1,11 +1,21 @@
 export function animate() {
     var sections = document.querySelectorAll(".section");
+    var background = document.querySelectorAll(".with-background");
+
+    var windowHeight = window.innerHeight;
+    var elementVisible = 100;
+
     for (var i = 0; i < sections.length; i++) {
-      var windowHeight = window.innerHeight;
       var elementTop = sections[i].getBoundingClientRect().top;
-      var elementVisible = 100;
       if (elementTop < windowHeight - elementVisible) {
         sections[i].classList.add("animate");
+      }
+    }
+
+    for (var i = 0; i < background.length; i++) {
+      var elementTop = background[i].getBoundingClientRect().top;
+      if (elementTop < windowHeight - elementVisible) {
+        background[i].classList.add("zoom-in");
       }
     }
 }
