0c5ff3be2152ba22bc23a6bd3395829a6757bc73
David Sánchez González <david@escenit.com>
2022-04-28 09:57:07 +0200

    added image carrousel to delivery note success case

--- src/app/ui/pages/success-cases/success-cases.page.ts
+++ src/app/ui/pages/success-cases/success-cases.page.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { animate } from 'src/app/shared/animate-on-scroll';
 
 @Component({
   selector: 'app-success-cases',
   templateUrl: './success-cases.page.html',
-  styleUrls: ['./success-cases.page.scss']
+  styleUrls: [
+    './success-cases.page.scss',
+    '../../../../theme/w3.css'
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class SuccessCasesPage implements OnInit {
   deliveryNoteImgs: string[] = [
@@ -18,10 +22,44 @@ export class SuccessCasesPage implements OnInit {
     '../../../../assets/success-cases/client-space-2.png',
   ];
 
+  slideIndex = 0;
+
   constructor() { }
 
   ngOnInit(): void {
     window.addEventListener('scroll', animate, true);
+  }
+
+  ngAfterViewInit() {
+    this.showDivs();
+  }
+
+  public plusDivs(n: number) {
+    this.showDivs(this.slideIndex + n);
+  }
+
+  showDivs(next: number | undefined = undefined) {
+    let imgs = document.getElementsByClassName('dn-img');
+
+    if (next) {
+      if (next >= imgs.length) {
+        next = 0;
+      }
+
+      if (next < 0) {
+        next = imgs.length - 1;
+      }
+
+      imgs[this.slideIndex].classList.add('hide-img');
+      imgs[next].classList.remove('hide-img');
+
+      this.slideIndex = next;
+    }
+
+    else {
+      imgs[this.slideIndex].classList.remove('hide-img');
+    }
+
   }
 
 }
